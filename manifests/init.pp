ecc15a67783270934782eb828c1996f42cdbf3a2
rcolbert <r.p.colbert@gmail.com>
2020-05-08 20:05:39 -0400

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,4 +5,25 @@
 # @example
 #   include elk
 class elk {
+  class {'logstash':}
+  logstash::plugin {'logstash-input-beats':}
+  logstash::configfile{'beats':
+    source => 'puppet:///modules/elk/beats.conf',
+  }
+
+
+  include ::java
+  class { 'elasticsearch':
+    jvm_options => ['-Xms256m','-Xmx256m']  
+  }
+  elasticsearch::instance { 'es-01': }
+
+  class {'kibana': 
+    config => {
+      'server.host' => '0.0.0.0'
+    }
+  }
+
+
+  include elk::filebeat
 }
